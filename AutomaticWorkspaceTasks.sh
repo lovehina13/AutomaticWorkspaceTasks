bb709b7cc219689095f58074d903de6976674740
Alexis Foerster <alexis.foerster@gmail.com>
2020-03-04 00:00:00 +0100

    Définition des variables CMAKE_BUILD_TYPE, CMAKE_CXX_FLAGS_DEBUG et CMAKE_CXX_FLAGS_RELEASE.

--- AutomaticWorkspaceTasks.sh
+++ AutomaticWorkspaceTasks.sh
@@ -15,43 +15,50 @@ do
     PROJECT_INSTALL_DIR="${WORKSPACE_INSTALL_DIR}/${PROJECT}"
 
     if [ ! -d ${PROJECT_SOURCE_DIR} ]; then git clone https://github.com/lovehina13/${PROJECT} ${PROJECT_SOURCE_DIR}; fi
-    if [ ! -d ${PROJECT_BUILD_DIR} ]; then mkdir -p ${PROJECT_BUILD_DIR}; fi
-    if [ ! -d ${PROJECT_INSTALL_DIR} ]; then mkdir -p ${PROJECT_INSTALL_DIR}; fi
 
     # Clean
     cd ${PROJECT_SOURCE_DIR}
-    git clean -fx -d
-    git reset --hard
-    git gc --aggressive
+#   git clean -fx -d
+#   git reset --hard
+#   git gc --aggressive
     git fetch --all --prune
     git pull --all
 
-    # Compile and install
-    PLATFORM="linux"
-    ARCHITECTURE="x64"
-    for C_COMPILER in gcc clang
-    do
+    if [ -f "${PROJECT_SOURCE_DIR}/CMakeLists.txt" ]
+    then
 
-        if [ ${C_COMPILER} == "gcc" ]; then CXX_COMPILER="g++"; CXX_FLAGS="-pedantic -Wall -Wextra -Wconversion -Wsign-conversion -Wold-style-cast"; fi
-        if [ ${C_COMPILER} == "clang" ]; then CXX_COMPILER="clang++"; CXX_FLAGS="-Weverything"; fi
+        if [ ! -d ${PROJECT_BUILD_DIR} ]; then mkdir -p ${PROJECT_BUILD_DIR}; fi
+        if [ ! -d ${PROJECT_INSTALL_DIR} ]; then mkdir -p ${PROJECT_INSTALL_DIR}; fi
 
-        for BUILD in debug release
+        # Compile and install
+        PLATFORM="linux"
+        ARCHITECTURE="x64"
+        for C_COMPILER in gcc clang
         do
 
-            TARGET="${PLATFORM}-${C_COMPILER}-${ARCHITECTURE}"
-            TARGET_BUILD_DIR="${PROJECT_BUILD_DIR}/${TARGET}/${BUILD}"
-            TARGET_INSTALL_DIR="${PROJECT_INSTALL_DIR}/${TARGET}/${BUILD}"
+            if [ ${C_COMPILER} == "gcc" ]; then CXX_COMPILER="g++"; CXX_FLAGS="-pedantic -Wall -Wextra -Wconversion -Wsign-conversion -Wold-style-cast"; fi
+            if [ ${C_COMPILER} == "clang" ]; then CXX_COMPILER="clang++"; CXX_FLAGS="-Weverything -Wno-padded -Wno-c++98-compat"; fi
+            CXX_FLAGS_DEBUG="-O0 -g"
+            CXX_FLAGS_RELEASE="-O2 -s"
 
-            if [ ! -d ${TARGET_BUILD_DIR} ]; then mkdir -p ${TARGET_BUILD_DIR}; fi
-            if [ ! -d ${TARGET_INSTALL_DIR} ]; then mkdir -p ${TARGET_INSTALL_DIR}; fi
+            for BUILD in debug release
+            do
 
-            cd ${TARGET_BUILD_DIR}
-            cmake ${PROJECT_SOURCE_DIR} -DCMAKE_INSTALL_PREFIX=${TARGET_INSTALL_DIR} -DCMAKE_C_COMPILER=${C_COMPILER} -DCMAKE_CXX_COMPILER=${CXX_COMPILER} -DCMAKE_CXX_FLAGS="${CXX_FLAGS}"
-            # cmake ${PROJECT_SOURCE_DIR} -G "Eclipse CDT4 - Unix Makefiles" -DCMAKE_ECLIPSE_GENERATE_SOURCE_PROJECT=TRUE -DCMAKE_ECLIPSE_VERSION=4.12 -DCMAKE_ECLIPSE_MAKE_ARGUMENTS=-j8 -DCMAKE_INSTALL_PREFIX=${TARGET_INSTALL_DIR} -DCMAKE_C_COMPILER=${C_COMPILER} -DCMAKE_CXX_COMPILER=${CXX_COMPILER} -DCMAKE_CXX_FLAGS="${CXX_FLAGS}"
-            cmake --build . --config ${BUILD} --target install
+                TARGET="${PLATFORM}-${C_COMPILER}-${ARCHITECTURE}"
+                TARGET_BUILD_DIR="${PROJECT_BUILD_DIR}/${TARGET}/${BUILD}"
+                TARGET_INSTALL_DIR="${PROJECT_INSTALL_DIR}/${TARGET}/${BUILD}"
+
+                if [ ! -d ${TARGET_BUILD_DIR} ]; then mkdir -p ${TARGET_BUILD_DIR}; fi
+                if [ ! -d ${TARGET_INSTALL_DIR} ]; then mkdir -p ${TARGET_INSTALL_DIR}; fi
+
+                cd ${TARGET_BUILD_DIR}
+                cmake ${PROJECT_SOURCE_DIR} -G "Eclipse CDT4 - Unix Makefiles" -DCMAKE_ECLIPSE_VERSION=4.12 -DCMAKE_INSTALL_PREFIX=${TARGET_INSTALL_DIR} -DCMAKE_BUILD_TYPE=${BUILD} -DCMAKE_C_COMPILER=${C_COMPILER} -DCMAKE_CXX_COMPILER=${CXX_COMPILER} -DCMAKE_CXX_FLAGS="${CXX_FLAGS}" -DCMAKE_CXX_FLAGS_DEBUG="${CXX_FLAGS_DEBUG}" -DCMAKE_CXX_FLAGS_RELEASE="${CXX_FLAGS_RELEASE}"
+                cmake --build . --target install
+
+            done
 
         done
 
-    done
+    fi
 
 done
